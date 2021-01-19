96e004e54e8858f96d0aa43d05b141503adcaa8c
reulyson <reulyson@gmail.com>
2021-01-19 18:39:17 -0400

    Enviando dados digitados entre duas activity.

--- app/src/main/java/com/example/introducao_android/ResultActivity.kt
+++ app/src/main/java/com/example/introducao_android/ResultActivity.kt
@@ -1,11 +1,18 @@
 package com.example.introducao_android
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val nomeDigitado = intent.getStringExtra("NOME_DIGITADO")
+
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        tvResult.text = nomeDigitado
     }
 }
